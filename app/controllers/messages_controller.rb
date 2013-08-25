535283b28b3a487d82af70610106e62462a71af1
Nachiket Kumar <nk@nachiketkumar.com>
2013-08-25 14:29:22 -0400

    add actions in messages controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,20 @@
 class MessagesController < ApplicationController
+
+  respond_to :json
+
+  def index
+    @messages = Message.all
+  end
+
+  def create
+    @message = Message.new(message_params)
+    @message.save
+    respond_with @message
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:owner, :content)
+  end
 end
